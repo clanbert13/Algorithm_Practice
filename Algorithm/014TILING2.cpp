e0e0c0f0f50837651edbf9a412dd1c99f0bb18ed
clanbert13 <clanbert13@gmail.com>
2026-01-14 18:02:06 +0900

    014 타일링 방법의 수 세기 문제 해결

---
+++ Algorithm/014TILING2.cpp
@@ -0,0 +1,67 @@
+// /*
+// 252p 8.12 타일링 방법의 수 세기
+
+// 문제
+// 2xn 크기의 사각형을 2x1 크기의 사각형으로 빈틈없이 채우는 경우의 수를 구하는 프로그램을 작성하세요.
+// 예를 들어 n=5라고 하면 다음 그림과 같이 여덟 가지의 방법이 있습니다.
+// 경우의 수는 n이 커지면 아주 커질 수 있으므로, 1000000007으로 나눈 값을 대신 출력하세요.
+
+// 입력
+// 입력의 첫 줄에는 테스트 케이스의 수(C <= 50)가 주어집니다. 그후 C줄에 각각 1개의 자연수로 n(1 <= n <= 100)이 주어집니다.
+
+// 출력
+// 각 테스트 케이스마다 한 줄에 경우의 수를 1000000007로 나눈 나머지를 출력합니다.
+
+// 예제 입력
+// 3
+// 1
+// 5
+// 100
+
+// 예제 출력
+// 1
+// 8
+// 782204094
+// */
+// #include <iostream>
+// #include <vector>
+// #include <cstring>
+
+// using namespace std;
+
+// const int MOD = 1000000007;
+// int cache[101];
+
+// int tiling(int width) {
+//   // 기저 사례: 너비가 1 이하인 경우
+//   if (width <= 1) return 1;
+
+//   // 메모이제이션
+//   int& result = cache[width];
+//   if (result != -1) return result;
+
+//   // 점화식: D[n] = D[n-2] + D[n-1]
+//   return result = (tiling(width - 2) + tiling(width - 1)) % MOD;
+// }
+
+// int main() {
+//   vector<int> res;
+//   int C;
+//   cin >> C;
+  
+  
+//   while (C--) {
+//     // 캐시 초기화
+//     memset(cache, -1, sizeof(cache));
+    
+//     int n;
+//     cin >> n;
+
+//     res.push_back(tiling(n));
+//   }
+
+//   for(int i : res)
+//     cout << i << '\n';
+
+//   return 0;
+// }
